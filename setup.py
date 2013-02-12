48d6738946abbb9bb3a10a4d28e5586b9c50426c
Casey Bajema <casey@bajtech.com.au>
2013-02-12 17:17:08 +1000

    Updated requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from setuptools import setup
- 
+
+requires = [
+    "pysandbox",
+    "Twisted",
+    "sqlalchemy",
+    "ipython",
+    "jcu.dc24.ingesterapi",
+    ]
+
 setup(name='jcu.dc24.ingesterplatform',
       version='0.0.1dev',
       description='DC24 Ingester Platform',
@@ -8,10 +16,10 @@ setup(name='jcu.dc24.ingesterplatform',
       url='http://www.coastalcoms.com',
       packages=['dc24_ingester_platform'],
       zip_safe=False,
+      install_requires=requires,
       entry_points={
           "console_scripts": ["mgmt_client = dc24_ingester_platform.client:main",
           "run_ingester = dc24_ingester_platform.ingester.data_sources:main"]
       },
-      install_requires = ["pysandbox", "jcu.dc24.ingesterapi"]
 )
 #      package_data={'twisted.plugins': ['twisted/plugins/dc24_ingester_platform.py']},
